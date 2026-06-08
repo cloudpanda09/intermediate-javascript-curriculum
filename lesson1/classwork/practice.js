e643b2819643122cf25b58c7692e0c55360f69da
cloudpanda09 <cloudpanda09@gmail.com>
2026-06-08 01:23:04 +0000

    Finished Classwork and HW 1

--- lesson1/classwork/practice.js
+++ lesson1/classwork/practice.js
@@ -2,20 +2,63 @@
 // Create variables for a student's name, age, and grade.
 // Print a sentence using all 3 variables.
 
+let name = "Alice";
+let age = 12;
+let grade = 93;
+
+console.log(name, "is", age, "years old and has a grade of", grade);
 
 // Problem 2
 // Create a variable for a score.
 // Print "Excellent" if the score is at least 90, "Good" if it is at least 70, otherwise print "Practice more".
 
+let score = 96;
+
+if (score >= 90) {
+    console.log("Excellent!");
+} else if (score >= 70) {
+    console.log("Good");
+} else {
+    console.log("Practice more.");
+}
 
 // Problem 3
 // Create an array of 5 numbers.
 // Print every number using a for loop.
 
+numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
 
 // Problem 4
 // Count how many numbers in the array are greater than 10.
 
+let nums = [10, 15, 5, 25, 63, 6]
+
+let count = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 10) {
+        count++
+    }
+}
+console.log("There are", count, "numbers greater than 10.");
 
 // Problem 5
 // Write a function that takes an array and returns the biggest number.
+
+nums2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function biggest_number(arr) {
+    let biggest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > biggest) {
+            biggest = arr[i];
+        }
+    }
+    return biggest;
+}
+
+console.log(biggest_number(nums2));
--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -2,20 +2,59 @@
 // Create variables for your favorite food, favorite color, and favorite number.
 // Print them in 3 separate sentences.
 
+let favorite_food = "pasta"
+let favorite_color = "purple"
+let favorite_number = "10"
+
+console.log("My favorite food is " + favorite_food + ". My favorite color is " + favorite_color + "and my favorite number is " + favorite_number)
 
 // Problem 2
 // Create a variable for a number.
 // Print whether it is positive, negative, or zero.
 
+let number = 53
+
+if (number > 0) {
+    console.log("The number is positive!")
+} else if (number == 0) {
+    console.log("The number is 0!")
+} else {
+    console.log("The number is negative!")
+}
 
 // Problem 3
 // Create an array of 5 animals.
 // Print the first animal, last animal, and length of the array.
 
+let animals = ["red panda", "dog", "cat", "hamster", "parakeet"]
+console.log(animals[0], animals[4])
 
 // Problem 4
 // Find the sum of all numbers in the array.
 
+let numbers = [1, 2, 3, 4, 5]
+
+let count = 0
+for (i = 0; i < numbers.length; i++) {
+    count = count + numbers[i];
+}
+
+console.log(count)
 
 // Problem 5
 // Write a function that counts how many times "red" appears in an array.
+
+let colors = ["red", "green", "blue", "yellow", "red"]
+
+function find_red(colors) {
+    let count = 0;
+
+    for (let i = 0; i < colors.length; i++) {
+        if (colors[i] === "red") {
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(find_red(colors));
